5d83e8c97cd026d36a3f1e14eb84a69c0805a842
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2022-12-20 01:56:48 +0900

    Create webpack.config.prod.js

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin=require("mini-css-extract-plugin");
+
+module.exports = {
+  mode:'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[chunkhash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins:[
+    new MiniCssExtractPlugin()
+  ],
+  devServer:{
+    hot:true
+  },
+ 
+
+};
