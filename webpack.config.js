5ea2755530d37919862ebabb847f6aa675907be0
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2022-12-20 03:28:50 +0900

    js rule 추가

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        test: /[\.js]$/, // .js 에 한하여 babel-loader를 이용하여 transpiling
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts$/, // .ts 에 한하여 ts-loader를 이용하여 transpiling
+        exclude: /node_module/,
+        use: {
+          loader: "ts-loader",
+        },
       },
       {
         test: /\.css$/,
