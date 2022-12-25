3a4ad7421164f386129ab7247d737a1e35639ece
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2022-12-25 18:19:00 +0900

    feat:create board

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import  './styles/style.css';
 
 
-function Home(){
+
+
+function render(){
   const root=document.getElementById('root')  as HTMLElement;
   
-  const container=document.createElement('div');
-  container.setAttribute('class','container-bg');
+  const onDragEnd=(e:any)=>{
+    console.log( e.target.style.backgroundColor);
+    e.target.style.backgroundColor="#ED1A25";
+  }
+  // 10*10 크기의 보드생성
+  [1,1,1,1,1,1,1,1,1,1].forEach(()=>{
+    const rowContainer=document.createElement('div');
+    rowContainer.setAttribute('class','row-container');
+    [1,1,1,1,1,1,1,1,1,1].forEach(()=>{
+  
+      const tile=document.createElement('div');
+      tile.setAttribute('class','tile');
+      tile.addEventListener("",onDragEnd);
+      rowContainer.appendChild(tile);
+  
+    });
+    root.appendChild(rowContainer);
+  });
 
-  root.appendChild(container);
+  
 }
 
 
 
 
-Home();
+render();
 
 
 
