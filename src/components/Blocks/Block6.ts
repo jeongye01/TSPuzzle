a09105d44ea52c3af1bb11ac06d5295e9d32b4aa
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2022-12-30 04:56:09 +0900

    refact : block6

--- src/components/Blocks/Block6.ts
+++ src/components/Blocks/Block6.ts
@@ -1,62 +1,65 @@
 import { blockInfo, boardState, diff, root } from "../..";
-import BlockContainer from "../blockContainer";
-import BlockTile from "../BlockTile";
+import BlockGenerator from "../BlockGenerator";
+import { statesSetter,calcBlockOriginPos } from "../blockDragStart";
 
-export function Block6 (diffSetter:(x:number,y:number)=>void,blockSetter:(block:HTMLDivElement,fillFunc:(x:number,y:number)=>void,overFunc:(x:number,y:number)=>void)=>void){
+
+
+const blockShape=[[1,1],[1,1]];
+export function Block6 (){
  
-    const block=document.createElement('div');
-    const blockRowContainer=document.createElement('div');
-    const blockRowContainer2=document.createElement('div');
-    const onDragStart=(e)=>{
-     
-     
-        // main block 좌표와 마우스 좌표 차이 계산
-        console.log(e.offsetX,e.offsetY)
-        const diffX=e.offsetX-60;
-        const diffY=e.offsetY-20;
-        const bindDiffSetter=diffSetter.bind(diff);
-        bindDiffSetter(diffX,diffY)
-        const bindBlockSetter=blockSetter.bind(blockInfo);
-        bindBlockSetter(block,fillBlock,overBlock)
-        // console.log("drag start");
-      }
+  const block=document.createElement('div');
+  
+  
+  const onDragStart=(e)=>{
    
-      BlockContainer(blockRowContainer);
-      BlockContainer(blockRowContainer2);
-    block.appendChild(blockRowContainer);
-    block.appendChild(blockRowContainer2);
-    block.draggable=true;
-    block.addEventListener("dragstart" ,onDragStart);
-    block.setAttribute('class','-block--six block');
+   
+    const {diffX,diffY} =calcBlockOriginPos(e.offsetX,e.offsetY,blockShape);
+    statesSetter(diffX,diffY,block,fillBlock,overBlock);
+    }
+  BlockGenerator(block,blockShape);
 
-   // block.setAttribute('class','block')
-   root.appendChild(block);
-   
-   }
+  block.draggable=true;
+  block.addEventListener("dragstart" ,onDragStart);
+  block.setAttribute('class','-block--four block');
+
+ 
+ // block.setAttribute('class','block')
+ root.appendChild(block);
+ 
+ }
 export const fillBlock =(x:number,y:number)=>{
-    console.log(x,y);
-  if(x-1<0 || y+1>9)return;
-  console.log(boardState[x][y] , boardState[x-1][y] , boardState[x-1][y+1])
-  if(boardState[x][y] || boardState[x][y+1] || boardState[x-1][y] || boardState[x-1][y+1]) return;
+console.log(x,y);
+if(x-1<0 || y+1>9)return;
+console.log(boardState);
+for(let ox=0; ox<blockShape[0].length;ox++){
+  for(let oy=0; oy<blockShape.length;oy++){
+     if(boardState[y+oy][x+ox-1] && blockShape[oy][ox] ) return;
+  }
+}
+for(let ox=0; ox<blockShape[0].length;ox++){
+for(let oy=0; oy<blockShape.length;oy++){
+  if(!blockShape[oy][ox]) continue;
+   boardState[y+oy][x+ox-1]=1;
+   document.getElementById(`${x+ox-1}+${y+oy}`).classList.add('tile-filled');
+   document.getElementById(`${x+ox-1}+${y+oy}`).classList.remove('tile-over');
+}
+}
 
-  boardState[x][y]=boardState[x][y+1]= boardState[x-1][y]=boardState[x-1][y+1]=1;
-  document.getElementById(`${x}+${y}`).classList.add('tile-filled');
-  document.getElementById(`${x}+${y+1}`).classList.add('tile-filled');
-  document.getElementById(`${x-1}+${y}`).classList.add('tile-filled');
-  document.getElementById(`${x-1}+${y+1}`).classList.add('tile-filled');
-  document.getElementById(`${x}+${y}`).classList.remove('tile-over');
-  document.getElementById(`${x}+${y+1}`).classList.remove('tile-over');
-  document.getElementById(`${x-1}+${y}`).classList.remove('tile-over');
-  document.getElementById(`${x-1}+${y+1}`).classList.remove('tile-over');
+
+
 }
 
 export const overBlock=(x:number,y:number)=>{
-    if(x-1<0 || y+1>9)return;
-    if(boardState[x][y] || boardState[x][y+1] || boardState[x-1][y] || boardState[x-1][y+1]) return;
-  
-   
-    document.getElementById(`${x}+${y}`).classList.add('tile-over');
-    document.getElementById(`${x}+${y+1}`).classList.add('tile-over');
-    document.getElementById(`${x-1}+${y}`).classList.add('tile-over');
-    document.getElementById(`${x-1}+${y+1}`).classList.add('tile-over');
+  if(x-1<0 || y+1>9)return;
+  for(let ox=0; ox<blockShape[0].length;ox++){
+    for(let oy=0; oy<blockShape.length;oy++){
+      if(boardState[y+oy][x+ox-1] && blockShape[oy][ox] ) return;
+    }
+ }
+ for(let ox=0; ox<blockShape[0].length;ox++){
+  for(let oy=0; oy<blockShape.length;oy++){
+    if(!blockShape[oy][ox]) continue;
+     document.getElementById(`${x+ox-1}+${y+oy}`).classList.add('tile-over');
+  }
+}
 }
--- src/index.ts
+++ src/index.ts
@@ -105,19 +105,19 @@ function render(){
   const blockContainer=document.createElement('div');
   
   
- // Block1();
- // Block2();
+ Block1();
+ Block2();
 Block3();
   Block4();
   Block5();
-/* Block6(diff.setter,blockInfo.setter);
+Block6();
  Block7(diff.setter,blockInfo.setter);
  Block8(diff.setter,blockInfo.setter);
   Block9(diff.setter,blockInfo.setter);
   Block10(diff.setter,blockInfo.setter); 
    Block11(diff.setter,blockInfo.setter); 
    Block12(diff.setter,blockInfo.setter);
-  Block13(diff.setter,blockInfo.setter);*/
+  Block13(diff.setter,blockInfo.setter);
   Block14();
   const blocks=document.querySelectorAll('.block');
 
