898e966ec84945b71c01f430b055dd5de8d71506
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2022-12-31 20:30:50 +0900

    Update isOutOfRange.ts

--- src/utils/isOutOfRange.ts
+++ src/utils/isOutOfRange.ts
@@ -1,5 +1,12 @@
 import { boardState } from "..";
 
+/**
+ * 블록일부가 보드 바깥으로 벗어난 경우 판단 
+ * @param x 변환된 구간 배열
+ * @param y mainder 선택되지 않은 구간 배열
+ * @param blockShape block 모양 데이터 
+ * @return 블록일부가 보드 바깥으로 벗어난 경우 true 아니면 false 리턴 
+ */
 export const isOutOfRange=(x:number,y:number,blockShape:number[][])=>{
     
     const rowLength=blockShape.length;
